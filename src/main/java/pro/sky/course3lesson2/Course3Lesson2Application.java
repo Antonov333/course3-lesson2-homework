655c2278602256ff9f7ddaf383a2b97203af5ea2
Antonov333 <antonovsergei333@yahoo.com>
2023-08-04 18:34:22 +0300

    Initial commit

---
+++ src/main/java/pro/sky/course3lesson2/Course3Lesson2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.course3lesson2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course3Lesson2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course3Lesson2Application.class, args);
+    }
+
+}
